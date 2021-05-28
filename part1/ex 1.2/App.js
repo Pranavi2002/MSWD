a1387e9adb639fc2807e716108e71c7b6cb136bc
Pranavi2002 <66314704+Pranavi2002@users.noreply.github.com>
2021-05-28 19:11:03 +0530

    App.js

--- part1/ex 1.2/App.js
+++ part1/ex 1.2/App.js
@@ -1,5 +1,15 @@
 import React from 'react'
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <p>
+        Hello the course name is {props.course}
+      </p>
+    </div>
+  )
+}
 const Part = (props) => {
   return (
     <div>
@@ -18,18 +28,32 @@ const Content =  (props) => {
   const exercises3 = 14
   return (
     <div>
+      <h2>Content</h2>
       <Part part={part1} exercise={exercises1}/>
       <Part part={part2} exercise={exercises2}/>
       <Part part={part3} exercise={exercises3}/>
-   
+      <Total  total={exercises1+exercises2+exercises3}/>
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        <h1>Total</h1>
+        Hello the total number of exercises is {props.total}
+      </p>
     </div>
   )
 }
 const App = () => {
+  const course = 'Half Stack application development'
   return(
     <div>
+      <Header course={course} />
       <Content />
     </div>
   )
 }
 export default App
+
